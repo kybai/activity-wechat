4a9fda80ac26281d2bdc102fa3eb44c882fcab52
ky.bai <834676597@qq.com>
2018-02-28 23:49:00 +0800

    修改图片上传下载方式，删除无用代码

--- src/main/java/com/activity/utils/OSSClientUtil.java
+++ src/main/java/com/activity/utils/OSSClientUtil.java
@@ -1,14 +1,12 @@
 package com.activity.utils;
 
 import com.aliyun.oss.OSSClient;
-import com.aliyun.oss.model.Bucket;
 import com.aliyun.oss.model.BucketInfo;
 import com.aliyun.oss.model.ObjectMetadata;
 import com.aliyun.oss.model.PutObjectResult;
 
-import java.io.*;
+import java.io.ByteArrayInputStream;
 import java.net.URL;
-import java.text.MessageFormat;
 import java.util.Date;
 
 /**
@@ -34,21 +32,6 @@ public class OSSClientUtil {
     }
 
     /**
-     * Base64图片上传，如果同名文件会覆盖服务器上的
-     *
-     * @param key        请使用UploadFile实体ID
-     * @param base64Str
-     * @param fileSuffix
-     * @return
-     */
-    public static PutObjectResult putBase64Img(String key, String base64Str, String fileSuffix) {
-        int i = base64Str.indexOf(",");
-        if (i != -1) base64Str = base64Str.substring(i + 1);
-        byte[] bytes = Base64ImageUtils.imageBase64StrToBytes(base64Str);
-        return putObject(key, bytes, fileSuffix);
-    }
-
-    /**
      * 对象上传，如果同名文件会覆盖服务器上的
      *
      * @param key        请使用UploadFile实体ID
@@ -64,17 +47,6 @@ public class OSSClientUtil {
         objectMetadata.setContentType(getContentType(fileSuffix));
         //objectMetadata.setContentDisposition("inline;filename=" + fileName);
         return ossClient.putObject(bucketName1, key, new ByteArrayInputStream(bytes), objectMetadata);
-    }
-
-    public static PutObjectResult putObjectUrl(String key, String url, String fileSuffix) throws IOException {
-        ObjectMetadata objectMetadata = new ObjectMetadata();
-//        objectMetadata.setContentLength(bytes.length);
-        objectMetadata.setCacheControl("no-cache");
-        objectMetadata.setHeader("Pragma", "no-cache");
-        objectMetadata.setContentType(getContentType(fileSuffix));
-
-        InputStream inputStream = new URL(url).openStream();
-        return ossClient.putObject(bucketName1, key , inputStream,objectMetadata);
     }
 
     /**
@@ -96,18 +68,6 @@ public class OSSClientUtil {
     }
 
     /**
-     * 获取文件的相对路径
-     *
-     * @param fileName 文件名称，格式如：abc.png
-     * @return 返回格式如：
-     */
-    public static String getObjectRelativePath(String fileName) {
-        Bucket bucket = bucketInfo1.getBucket();
-        String fileFullPath = MessageFormat.format("{0}.{1}/{2}", bucket.getName(), bucket.getExtranetEndpoint());
-        return fileFullPath;
-    }
-
-    /**
      * 获得对象url链接（此方法不会向OSS服务器发送请求，使用频率高也无妨）
      *
      * @param key
@@ -120,13 +80,10 @@ public class OSSClientUtil {
         // 生成URL
         URL url = ossClient.generatePresignedUrl(bucketName1, key, expiration);
 
-//        if (url != null)
-//            return    url.toString();
-
-        if (url != null){
+        if (url != null) {
             String sbustr = url.toString();
-            //TODO 页面中会把&符号转义 amp; 所以截取图片地址，？只要问号前的。
-            return sbustr.substring(0,sbustr.indexOf("?"));
+            // 页面中会把&符号转义 amp; 所以截取图片地址，？只要问号前的。
+            return sbustr.substring(0, sbustr.indexOf("?"));
         }
 
         return null;
@@ -174,23 +131,5 @@ public class OSSClientUtil {
     public void destory() {
         if (ossClient != null)
             ossClient.shutdown();
-    }
-
-    public static void main(String[] args) throws IOException {
-        String key = "354617";
-
-        File file = new File("E:/aa.png");
-        FileInputStream fis = new FileInputStream(file);
-
-//        OSSClientUtil.putObject(key, StreamUtil.readBytesFromInputStream(fis), "jpg");
-        OSSClientUtil.putObject("363801", StreamUtil.readBytesFromInputStream(fis), "jpg");
-        System.out.println(OSSClientUtil.getObjectUrl("363801"));
-        fis.close();
-//·
-//        System.out.println(OSSClientUtil.doesObjectExist(key));
-
-//        System.out.println(OSSClientUtil.getObjectUrl(key));
-
-//        OSSClientUtil.deleteObject(key);
     }
 }
